147df8a63f8ee6e0750c72e8abf87ccb4941461c
Facundo Arimany <arimanyfacundo@gmail.com>
2023-04-24 16:16:23 +0200

    Finished Array Cardio 2

--- Array Cardio 2.js
+++ Array Cardio 2.js
@@ -34,7 +34,15 @@ console.table(isEveryoneOlderThan19)
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
-
+const idComment823423 = comments.find((comment)=>{
+  return comment.id == 823423
+})
+console.log(idComment823423)
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+ const index = comments.findIndex((comment)=>{
+     return comment.id == 823423
+ }) 
+  comments.splice(index, 1)
+  console.table(comments)
